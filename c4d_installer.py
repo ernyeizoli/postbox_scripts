99f16902ddd3682586b442e86a8a735e19e17db2
Akos Kiss <akoskiss@Akoss-MacBook-Pro.local>
2025-05-15 13:01:54 +0200

    installer

--- c4d_installer.py
+++ c4d_installer.py
@@ -4,17 +4,19 @@ import platform
 
 def get_c4d_script_path():
     if platform.system() == "Darwin":
-        return os.path.expanduser("~/Library/Preferences/Maxon/Cinema 4D R26/library/scripts/")
+        return os.path.expanduser("~/Library/Preferences/Maxon/Maxon Cinema 4D 2025_FFA38A4B/library/scripts/")
     elif platform.system() == "Windows":
-        return os.path.join(os.getenv("APPDATA"), "Maxon", "Cinema 4D R26", "library", "scripts/")
+        return os.path.join(os.getenv("APPDATA"), "Maxon", "Maxon Cinema 4D 2025_FFA38A4B", "library", "scripts/")
     raise RuntimeError("Unsupported OS")
 
-def copy_folder(src, dst):
-    dst_folder = os.path.join(dst, os.path.basename(src))
-    if os.path.exists(dst_folder):
-        shutil.rmtree(dst_folder)
-    shutil.copytree(src, dst_folder)
-    print(f"✅ Installed {os.path.basename(src)} to {dst_folder}")
+def copy_files(src, dst):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for item in os.listdir(src):
+        src_item = os.path.join(src, item)
+        if os.path.isfile(src_item):
+            shutil.copy2(src_item, dst)
+            print(f"✅ Copied {item} to {dst}")
 
 def main():
     script_root = os.path.join("scripts", "C4d_Scripts")
@@ -23,7 +25,7 @@ def main():
     for folder in os.listdir(script_root):
         full_path = os.path.join(script_root, folder)
         if os.path.isdir(full_path):
-            copy_folder(full_path, dst_root)
+            copy_files(full_path, dst_root)
 
 if __name__ == "__main__":
     main()
